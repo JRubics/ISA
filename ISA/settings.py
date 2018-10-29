25e062dc2c3d3f4a6f3ab313cec064462baf0c2e
Jelena Dokic <jelena.dpk@gmail.com>
2018-10-29 18:12:48 +0100

    base template and bootstrap added

--- ISA/settings.py
+++ ISA/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 LOGIN_URL='/user/login/'
 LOGIN_REDIRECT_URL='/user/home/'
