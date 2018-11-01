5cae8a3d3da6ab7a2cf4663bfdb2e76360001923
Jelena Dokic <jelena.dpk@gmail.com>
2018-11-01 14:52:08 +0100

    user profile and sign in

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_submit, name='login_submit'),
+    path('register/', views.registration_submit, name='registration_submit'),
     path('logout/', views.logout_submit, name='logout_submit'),
     path('home/', views.home, name='home'),
 ]
--- user/views.py
+++ user/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.template import *
 from django.conf import settings
 from django.contrib.auth.models import User
+from .models import Profile
 
 def login_submit(request):
     if request.method == 'POST':
@@ -24,6 +25,28 @@ def login_submit(request):
     else:
         logout(request)
         return render(request,'user/login_page.html')
+
+def registration_submit(request):
+    if request.method == 'POST':
+        # DODAJ UNIQUE NA USERNAME I EMAIL
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        city = request.POST['city']
+        phone_number = request.POST['phone_number']
+        if password1 == password2:
+            user = User.objects.create_user(username=username, email=email, password=password1, first_name=first_name, last_name=last_name)
+            profile = Profile.objects.create(user=user, city=city, phone_number=phone_number)
+            return redirect('/user/home')
+        else:
+            messages.error(request, "Passwords must be same")
+            return render(request,'user/registration_page.html')
+    else:
+        logout(request)
+        return render(request,'user/registration_page.html')
 
 @login_required()
 def logout_submit(request):
