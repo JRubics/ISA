0824478501a244a2dfd561beba72d8121f40bef8
Jelena Dokic <jelena.dpk@gmail.com>
2018-11-11 17:19:41 +0100

    user tests

--- user/models.py
+++ user/models.py
@@ -10,8 +10,25 @@ class Profile(models.Model):
     )
     city = models.CharField(max_length=30)
     phone_number = models.CharField(max_length=30)
+
     class Meta:
         permissions = (("is_car_admin", "Is car admin"),
                        ("is_hotel_admin", "Is hotel admin"),
                        ("is_flight_admin", "Is flight admin"),
                        )
+
+    def get_username_from_email(email):
+        if User.objects.filter(email=email).exists():
+            return User.objects.get(email=email).username
+        else:
+            return None
+
+    def validate_user(username, email, password1, password2):
+        if User.objects.filter(username=username).exists():
+            return "Username already exists"
+        elif User.objects.filter(email=email).exists():
+            return "Email already exists"
+        elif password1 != password2:
+            return "Passwords must be same"
+        else:
+            return None
--- user/views.py
+++ user/views.py
@@ -13,7 +13,7 @@ from django.contrib.contenttypes.models import ContentType
 
 def login_submit(request):
     if request.method == 'POST':
-        user = authenticate(request, username=get_username_from_email(request.POST['email']), password=request.POST['password'])
+        user = authenticate(request, username=Profile.get_username_from_email(request.POST['email']), password=request.POST['password'])
         if user is not None:
             login(request, user)
             return redirect('/user/home')
@@ -24,15 +24,9 @@ def login_submit(request):
         logout(request)
         return render(request,'user/login_page.html')
 
-def get_username_from_email(email):
-    if User.objects.filter(email=email).exists():
-        return User.objects.get(email=email).username
-    else:
-        return None
-
 def registration_submit(request):
     if request.method == 'POST':
-        message = validate_user(request.POST['username'], request.POST['email'], request.POST['password1'], request.POST['password2'])
+        message = Profile.validate_user(request.POST['username'], request.POST['email'], request.POST['password1'], request.POST['password2'])
         if message:
             messages.error(request, message)
             return render(request,'user/registration_page.html')
@@ -50,17 +44,6 @@ def registration_submit(request):
     else:
         logout(request)
         return render(request,'user/registration_page.html')
-
-
-def validate_user(username, email, password1, password2):
-    if User.objects.filter(username=username).exists():
-        return "Username already exists"
-    elif User.objects.filter(email=email).exists():
-        return "Email already exists"
-    elif password1 != password2:
-        return "Passwords must be same"
-    else:
-        return None
 
 def send_email(username, email):
     send_mail(
