db657445686c2e8b56d17fd7783b16d68c8f95c5
Jelena Dokic <jelena.dpk@gmail.com>
2018-11-28 16:06:24 +0100

    urls

--- car/urls.py
+++ car/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('home', views.car_home, name='car_home'),
     path('service', views.add_service, name='add_service'),
     path('service/<id>', views.edit_service, name='edit_service'),
-    path('add_car/<service_id>', views.add_car, name='add_car'),
-    path('edit_car/<id>', views.edit_car, name='edit_car'),
-    path('add_office/<service_id>', views.add_office, name='add_office'),
-    path('edit_office/<id>', views.edit_office, name='edit_office'),
+    path('add/<service_id>', views.add_car, name='add_car'),
+    path('edit/<id>', views.edit_car, name='edit_car'),
+    path('office/<service_id>', views.add_office, name='add_office'),
+    path('office/edit/<id>', views.edit_office, name='edit_office'),
 ]
