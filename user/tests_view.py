c1a1608c1fb1a2bfe43b7307894d5aba3d206da0
Jelena Dokic <jelena.dpk@gmail.com>
2018-11-28 22:56:57 +0100

    selenium1

--- user/tests_view.py
+++ user/tests_view.py
@@ -8,6 +8,7 @@ from .models import Profile
 class UserSeleniumTestCase(LiveServerTestCase):
     def setUp(self):
       self.selenium = webdriver.Chrome()
+      super(UserSeleniumTestCase, self).setUp()
 
       self.user = User.objects.create(username='test', email='test@test.com', is_active=True)
       self.profile = Profile.objects.create(user=self.user, city='Novi Sad', phone_number='021333444')
@@ -16,12 +17,12 @@ class UserSeleniumTestCase(LiveServerTestCase):
 
     def tearDown(self):
       self.selenium.quit()
+      super(UserSeleniumTestCase, self).tearDown()
 
     def test_login(self):
       selenium = self.selenium
-      selenium.get('http://127.0.0.1:8000/user/login/')
+      selenium.get(self.live_server_url + "/user/login")
 
-      print(selenium.page_source)
       email = selenium.find_element_by_id('login_email')
       password = selenium.find_element_by_id('login_pass')
       submit = selenium.find_element_by_name('login')
