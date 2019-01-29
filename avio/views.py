840851b50478ae6c1aedc6d98ba8e025f714de1d
FJakovljevic <fillix96@gmail.com>
2019-01-29 01:45:09 +0100

    napravljek kompletni admin za za pravljenje sedista

--- avio/views.py
+++ avio/views.py
@@ -1,21 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
-from avio.models import Seat
+from avio.models import Seat, Flight
+from django import forms
+from django.db.models import Max
+from django.contrib import messages
+from django.http import Http404
 import json
+
+class SeatTypeForm(forms.ModelForm):
+    number = forms.IntegerField(min_value=1)
+    class Meta:
+        model = Seat
+        fields = ['seat_type']
+
+class SeatUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Seat
+        fields = ['seat_number', 'seat_type', 'seat_status']
 
 
 @login_required()
 @permission_required('user.is_avio_admin')
 def seat_change(request, id):
     querry = Seat.objects.filter(flight = id)
+    fligth = Flight.objects.get(pk=id)
     number_b = querry.filter(seat_type = 'B').order_by('seat_number').values_list('seat_status', 'seat_number')
     number_f = querry.filter(seat_type = 'F').order_by('seat_number').values_list('seat_status', 'seat_number')
     number_e = querry.filter(seat_type = 'E').order_by('seat_number').values_list('seat_status', 'seat_number')
     context = {}
+    context['id'] = id
     context['number_b'] = len(number_b)
     context['number_f'] = len(number_f)
     context['number_e'] = len(number_e)
     context['seats_e'] = json.dumps(list(number_e))
     context['seats_f'] = json.dumps(list(number_f))
     context['seats_b'] = json.dumps(list(number_b))
-    return render(request, 'avio/admin_seat_change.html', context)
+    context['form_seat'] = SeatTypeForm()
+    context['seat_update_form'] = SeatUpdateForm()
+    if request.method == 'POST':
+        add_seats = SeatTypeForm(request.POST)
+        update_seat = SeatUpdateForm(request.POST)
+        
+        if request.POST['action'] == 'Add':
+            if add_seats.is_valid():
+                seat_type = add_seats['seat_type'].value()
+                number = int(add_seats['number'].value())
+                max_seat_number = Seat.objects.filter(seat_type=seat_type).aggregate(max = Max('seat_number'))['max']
+                if max_seat_number == None:
+                    max_seat_number = 1
+                else:
+                    max_seat_number = max_seat_number + 1
+                price_factor = 1
+                if seat_type == "B":
+                    price_factor = 1.5
+                if seat_type == "F":
+                    price_factor = 2
+                for x in range(number):
+                    Seat.objects.create(flight=fligth, seat_number=(max_seat_number+x), seat_status="F", seat_type=seat_type, price_factor = price_factor)
+        else:
+            seat_type = update_seat['seat_type'].value()
+            seat_status = update_seat['seat_status'].value()
+            number = int(update_seat['seat_number'].value())
+            try:
+                obj = Seat.objects.get(flight = id, seat_type = seat_type, seat_number = number)
+                obj.seat_status = seat_status
+                obj.save()
+            except Seat.DoesNotExist:
+                raise Http404("No Seat matches the given query.")
+
+        return redirect('avio:seats_change', id)
+    else:
+        return render(request, 'avio/admin_seat_change.html', context)
