1375a4f665430b56f30fcf32c23efc5edaa588c6
FJakovljevic <fillix96@gmail.com>
2019-02-14 07:57:11 +0100

    Update admin.py

--- avio/admin.py
+++ avio/admin.py
@@ -32,7 +32,7 @@ class FlightAdmin (admin.ModelAdmin):
     def get_form(self, request, obj=None, **kwargs):
         form = super(FlightAdmin, self).get_form(request, obj, **kwargs)
         if not request.user.is_superuser:
-            form.base_fields['avio_company'].initial = request.user.adminuser.avio_admin
+            form.base_fields['avio_company'].initial = request.user.adminuser.avio_admin.id
             form.base_fields['avio_company'].disabled = True
         return form
 
